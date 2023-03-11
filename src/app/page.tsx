d12e35857a8d653216d0c7567d9bc27f552ca05e
Leonardo Cintra <leonardo.ncintra@outlook.com>
2023-03-10 22:30:03 -0300

    chore: chora nenem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
 import { Inter } from "next/font/google";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="">
-      <main>
-        <h1>HOME</h1>
-      </main>
-    </div>
+    <main className="flex">
+      <div className="p-4">
+        <h1 className="text-lg md:text-5xl">
+          Cristo Rei - implementos agricolas
+        </h1>
+        <span>Facilitando seu trabalho no campo.</span>
+        <br />
+        <span>
+          Implementos simples, práticos e muito eficientes para tornar seu
+          trabalho mais fácil
+        </span>
+      </div>
+      <Image
+        className="hidden md:block"
+        src={"/img/logo.png"}
+        height={500}
+        width={500}
+        alt={"logo"}
+      />
+    </main>
   );
 }
